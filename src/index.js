611df34c562c07e77c78e1d98d42805fbb9164fe
duongquangmanh <duong.quang.manh@sun-asterisk.com>
2019-07-10 16:23:04 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from "react-redux";
 import UserApp from './containers/userApp'
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import reducer from "./reducers/reducer";
+import thunk from 'redux-thunk';
 
 const initialState = {};
 
-const store = createStore(reducer, initialState);
+const store = createStore(reducer, initialState, applyMiddleware(thunk));
 
 const appRoot = (
     <Provider store={store}>
